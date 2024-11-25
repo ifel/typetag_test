4592cfa573d0f7514298f3e513ba34bfd3d114c4
Igor Kanyuka <ifelmail@gmail.com>
2024-11-25 21:19:13 +0000

    Initial test

---
+++ src/main.rs
@@ -0,0 +1,63 @@
+use std::sync::Arc;
+use std::rc::Rc;
+
+use serde::Serialize;
+use serde::Deserialize;
+use serde;
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct A {
+    f1: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct B {
+    f1: String,
+}
+
+#[typetag::serde(tag = "type")]
+trait MyTrait {
+    fn str(&self) -> String;
+}
+
+#[typetag::serde]
+impl MyTrait for A {
+    fn str(&self) -> String {
+        "This is A".to_string()
+    }
+}
+
+#[typetag::serde]
+impl MyTrait for B {
+    fn str(&self) -> String {
+        "This is B".to_string()
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+struct Container {
+    aaa: Arc<dyn MyTrait>,
+}
+
+fn main() -> serde_json::Result<()> {
+    let a = A{f1: "A".to_string()};
+    let b = B{f1: "B".to_string()};
+    println!("Hello, world!:\nA: {}\nB: {}", a.str(), b.str());
+    let a_trait = &a as &dyn MyTrait;
+    let a_trait_serialized = serde_json::to_string(a_trait)?;
+    println!("{a_trait_serialized}");
+    let b_trait = &b as &dyn MyTrait;
+    let b_trait_serialized = serde_json::to_string(b_trait)?;
+    println!("{b_trait_serialized}");
+
+    let de: Box<dyn MyTrait> = serde_json::from_str(&b_trait_serialized)?;
+    println!("{}", de.str());
+
+    let cont = Container{aaa: Arc::new(b)};
+    let cont_serialized = serde_json::to_string(&cont)?;
+    println!("{cont_serialized}");
+    let cont2 = serde_json::from_str::<Container>(&cont_serialized)?;
+    println!("{}", cont2.aaa.str());
+
+    Ok(())
+}
